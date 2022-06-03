e1663d3c777618f85010fd739facc833faedbea1
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-06-03 14:26:49 -0300

    Atualizando e estilizando em arquivo separado a página Home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#232630',
+  },
+
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 18,
+    marginTop: 18,
+    marginBottom: 24,
+  },
+
+  name: {
+    fontSize: 28,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+
+  categories: {
+    maxHeight: 115,
+    backgroundColor: '#efefef',
+    marginHorizontal: 18,
+    borderRadius: 8,
+    zIndex: 9,
+  },
+
+  main: {
+    backgroundColor: '#fff',
+    flex: 1,
+    marginTop: -30,
+  },
+
+  title: {
+    fontSize: 21,
+    paddingHorizontal: 18,
+    marginBottom: 14,
+    fontWeight: 'bold',
+    color: '#162133',
+  },
+});
