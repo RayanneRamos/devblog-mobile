427b6c2855e6b98478395102544d3943dabc529f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-06-03 14:25:42 -0300

    Atualizando e estilizando em arquivo separado o componente PostItem

---
+++ src/components/PostItem/index.jsx
@@ -0,0 +1,36 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { styles } from './styles';
+
+function PostItem({ data }) {
+
+  const navigation = useNavigation();
+
+  function handleDetails() {
+    navigation.navigate('Detail', { 
+      id: data?.id,
+    });
+  }
+
+  return(
+   <TouchableOpacity style={styles.container} onPress={handleDetails}>
+     <View style={styles.header}>
+        <Image 
+          style={styles.cover} 
+          source={{ uri: `http://192.168.0.5:1337${data?.attributes?.cover?.data?.attributes?.url}` }}
+        />
+     </View>
+     <View style={styles.body}>
+        <Text style={styles.title}>{data.attributes.title}</Text>
+        <Text 
+          style={styles.description}
+          numberOfLines={2}
+        >
+          {data.attributes.description}
+        </Text>
+     </View>
+   </TouchableOpacity>
+  );
+}
+
+export { PostItem };
